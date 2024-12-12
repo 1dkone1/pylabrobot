dc931f4eb71f2bfce756dd67c6e3effafa3c9bef
1dkone1 <119891122+1dkone1@users.noreply.github.com>
2024-12-12 18:11:35 -0500

    New pcr plate definition.

--- pylabrobot/resources/thermo_fisher/plates.py
+++ pylabrobot/resources/thermo_fisher/plates.py
@@ -245,3 +245,74 @@ def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_L(name: str, with_lid: bool = Fa
 
 def Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate_P(name: str, with_lid: bool = False) -> Plate:
   return Thermo_AB_96_wellplate_300ul_Vb_EnduraPlate(name=name, with_lid=with_lid).rotated(90)
+
+
+
+# # # # # # # # # # Thermo_FS_96_wellplate_200_Vb_semi-skirted # # # # # # # # # #
+# Pub No. MAN0014411
+
+
+def _compute_volume_from_height_Thermo_FS_96_wellplate_300ul_Vb_semi_skirted(
+    h: float,
+):
+  raise NotImplementedError("This function is not implemented yet")
+
+
+def _compute_height_from_volume_Thermo_FS_96_wellplate_300ul_Vb_semi_skirted(
+    liquid_volume: float,
+):
+  raise NotImplementedError("This function is not implemented yet")
+
+
+def Thermo_FS_96_wellplate_300ul_Vb_semi_skirted_Lid(name: str) -> Lid:
+  raise NotImplementedError("This lid is not currently defined.")
+
+
+
+
+def Thermo_FS_96_wellplate_300ul_Vb_semi_skirted(name: str, with_lid: bool = False) -> Plate:
+  """Thermo Fisher Scientific/Fisher Scientific cat. no.: AB0990.
+  - See `./engineering_diagrams/` directory for more part numbers.
+  - Material: Polypropylene.
+  - Sterilization compatibility: ?
+  - Chemical resistance: ?
+  - Thermal resistance: ?
+  - Cleanliness: 'Certified RNase/DNase-free'.
+  - Format: ??? (Maybe: ANSI/SLAS-format for compatibility with automated systems.)
+  - ??? (Maybe: optimal pickup_distance_from_top=4 mm.)
+  - total_volume = 300 ul.
+  - working_volume = 200 ul (recommended by manufacturer).
+  - Cut corner: H1.
+  - Can be cut into 24 and 48 well sections.
+  """
+  return Plate(
+    name=name,
+    size_x=120.0,
+    size_y=80.0,
+    size_z=19.5 + 0.6 - 0.5,  # cavity_depth(19.5) + material_z_thickness(0.6) - well_extruding_over_plate(0.5)
+    lid=Thermo_FS_96_wellplate_300ul_Vb_semi_skirted_Lid(name + "_lid") if with_lid else None,
+    model="Thermo_FS_96_wellplate_300ul_Vb_semi_skirted",
+    plate_type="semi-skirted",
+    ordered_items=create_ordered_items_2d(
+      Well,
+      num_items_x=12,
+      num_items_y=8,
+      dx=11.63,
+      dy=9.95,
+      dz=0.0,  # check that plate is semi-skirted
+      item_dx=9,
+      item_dy=9,
+      size_x=5.49,
+      size_y=5.49,
+      size_z=20.1,
+      bottom_type=WellBottomType.V,
+      material_z_thickness=1.6,
+      cross_section_type=CrossSectionType.CIRCLE,
+      compute_volume_from_height=(
+        _compute_volume_from_height_Thermo_FS_96_wellplate_300ul_Vb_semi_skirted
+      ),
+      compute_height_from_volume=(
+        _compute_height_from_volume_Thermo_FS_96_wellplate_300ul_Vb_semi_skirted
+      ),
+    ),
+  )
